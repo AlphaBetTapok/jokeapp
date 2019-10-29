a77407aa5928f48ed77d9494d704ee0d46796f25
Антон Тараненко <taranenkoant@gmail.com>
2019-10-29 14:14:36 +0300

    initial commit

---
+++ src/test/java/com/taranenkoant/joke/joke/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taranenkoant.joke.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
